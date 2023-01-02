216d1a3252771377feeac73f48eb66af8c473f6c
Gareth <bravdthepally@gmail.com>
2023-01-02 14:15:54 +0200

    Added install tasks

--- src/install.sh
+++ src/install.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 
+script_dir=$(dirname "$0")
+
 while true; do
   echo "Please choose a number:"
-  echo "1. Placeholder function 1"
-  echo "2. Placeholder function 2"
-  echo "3. Placeholder function 3"
-  echo "4. Placeholder function 4"
+  echo "1. Signal"
+  echo "2. Flatpak"
+  echo "3. Brave"
+  echo "e. Full new system on deb install"
   echo "q. Quit"
 
-  read choice
+  read -r choice
 
   case $choice in
     1)
-      # Placeholder function 1
-      echo "You selected 1"
+      echo "Installing Signal"
+      bash "$script_dir/../apps/signal.sh"
       ;;
     2)
-      # Placeholder function 2
-      echo "You selected 2"
+      echo "Installing Flatpak"
+      bash "$script_dir/../apps/flatpak.sh"
       ;;
     3)
-      # Placeholder function 3
-      echo "You selected 3"
+      echo "Installing Brave Browser"
+      bash "$script_dir/../apps/brave.sh"
       ;;
-    4)
-      # Placeholder function 4
-      echo "You selected 4"
+    e)
+      echo "Starting install process for a new DEB based system."
+      bash "$script_dir/../apps/new-ubuntu-system.sh"
       ;;
     q)
       break
       ;;
     *)
-      echo "Invalid choice"
+      printf "Invalid choice\n\n"
       ;;
   esac
 done
