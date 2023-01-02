23bc88284cf1255787f521b793a5e2ac86aaabc9
Gareth <bravdthepally@gmail.com>
2023-01-02 14:22:23 +0200

    Updated install methods

--- apps/signal.sh
+++ apps/signal.sh
@@ -6,4 +6,4 @@ cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
 sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
 
-sudo apt install signal-desktop -yy
+sudo apt update && sudo apt install signal-desktop -yy
